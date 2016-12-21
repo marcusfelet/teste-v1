55512d543e7c85429a5ba952a1d8508bfcdf8870
KennyMack <creativeweb.de@gmail.com>
2016-12-21 15:26:57 -0200

    working in frontend 85% - (home and detail 100%, search 100%, admin 95%)

--- public/js/controller/SearchController.js
+++ public/js/controller/SearchController.js
@@ -10,6 +10,8 @@
     vm.gridList = hotelsData.data.msg;
     vm.getStar = getStar;
 
+    vm.searchStr = '';
+
 
     function getStar(indice, star) {
       return (star >= indice ) ? 'fa fa-star' : 'fa fa-star-o';
